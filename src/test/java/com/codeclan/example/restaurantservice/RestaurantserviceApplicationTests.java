c2143ab2065a964dc5ccf76f1f5496064e92e5da
Cameron Moir <cameronjmoir@gmail.com>
2019-12-15 15:52:20 +0000

    can get restaurants

--- src/test/java/com/codeclan/example/restaurantservice/RestaurantserviceApplicationTests.java
+++ src/test/java/com/codeclan/example/restaurantservice/RestaurantserviceApplicationTests.java
@@ -1,6 +1,7 @@
 package com.codeclan.example.restaurantservice;
 
 import com.codeclan.example.restaurantservice.models.Customer;
+import com.codeclan.example.restaurantservice.models.Restaurant;
 import com.codeclan.example.restaurantservice.repositories.CustomerRepository;
 import com.codeclan.example.restaurantservice.repositories.DishRepository;
 import com.codeclan.example.restaurantservice.repositories.OrderRepository;
@@ -39,6 +40,11 @@ class RestaurantserviceApplicationTests {
 	@Test
 	public void canGetCustomers(){
 		List<Customer> found = customerRepository.findAll();
+		assertEquals(1, found.size());
+	}
+	@Test
+	public void canGetRestaurants(){
+		List<Restaurant> found = restaurantRepository.findAll();
 		assertEquals(1, found.size());
 	}
 
