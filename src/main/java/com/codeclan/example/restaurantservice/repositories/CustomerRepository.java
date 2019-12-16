dc16b0d9fc0f84401654d80703cca24c192f732e
Cameron Moir <cameronjmoir@gmail.com>
2019-12-16 13:32:36 +0000

    projection for dish completed

--- src/main/java/com/codeclan/example/restaurantservice/repositories/CustomerRepository.java
+++ src/main/java/com/codeclan/example/restaurantservice/repositories/CustomerRepository.java
@@ -2,11 +2,12 @@ package com.codeclan.example.restaurantservice.repositories;
 
 
 import com.codeclan.example.restaurantservice.models.Customer;
+import com.codeclan.example.restaurantservice.projections.EmbedOrderAndRestaurant;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.stereotype.Repository;
 
-@RepositoryRestResource()
+@RepositoryRestResource(excerptProjection = EmbedOrderAndRestaurant.class)
 public interface CustomerRepository extends JpaRepository<Customer, Long> {
 
 }
